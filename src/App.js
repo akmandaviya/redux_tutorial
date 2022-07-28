a37213476e790639247842a6f73d508ebb84e9c2
akmandaviya <abdulmandaviya@gmail.com>
2022-07-28 22:44:58 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='box'>
+      <h1>Increment and Decrement Counter</h1>
+      <h3>Using React-Redux</h3>
+
+      <div>
+        <a className='negative' href='/' title='Decrement'><span>Incre</span></a>
+        <input name='item' type='text' className='item-input' value='0'/>
+        <a className='positive' href='/' title='Increment'><span>Decre</span></a> 
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+
